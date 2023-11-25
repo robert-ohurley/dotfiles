dfffedd493d33c00d7ff3f88e4e36ffa7c8ad375
RojetSavage <robert-hurley@hotmail.com.au>
2023-11-26 09:48:15 +1100

    update shell script to remove directories replaced by symlink

--- setup.sh
+++ setup.sh
@@ -1,15 +1,24 @@
 #tmux
-ln -s ~/dotfiles/tmux ~/.config/tmux
+ln -s -f ~/dotfiles/tmux ~/.config/
 
 #nvim
-ln -s ~/dotfiles/nvim ~/.config/nvim
+#remove directories first before replacing with symlinks
+rm -rf ~/.config/nvim/after
+rm -rf ~/.config/nvim/core
+rm -rf ~/.config/nvim/lua
+
+ln -s -f ~/dotfiles/nvim/after ~/.config/nvim/
+ln -s -f ~/dotfiles/nvim/core ~/.config/nvim/
+ln -s -f ~/dotfiles/nvim/lua ~/.config/nvim/
 
 #zsh
-ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/dotfiles/zsh/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
+ln -s -f ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s -f ~/dotfiles/zsh/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
 
 #git
-ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
+ln -s -f ~/dotfiles/git/.gitconfig ~/.gitconfig
 
 #vscode
-ln -s ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+ln -s -f ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+
+echo "Completed symlinking config files"
