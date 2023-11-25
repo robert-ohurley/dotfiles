e5a5f92faf9571172b9535348743a885fdb11bae
RojetSavage <robert-hurley@hotmail.com.au>
2023-11-26 09:21:46 +1100

    add initial setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#tmux
+ln -s ~/dotfiles/tmux ~/.config/tmux
+
+#nvim
+ln -s ~/dotfiles/nvim ~/.config/nvim
+
+#zsh
+ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/dotfiles/zsh/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
+
+#git
+ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
+
+#vscode
+ln -s ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
