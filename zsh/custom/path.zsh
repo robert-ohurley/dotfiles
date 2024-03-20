f84af52e99e71adfe8fac89a011194777b50fd41
RojetSavage <robert-hurley@hotmail.com.au>
2024-03-20 18:57:00 +1100

    add nvm to path

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -5,3 +5,8 @@ export PATH=/home/rojetsavage/.local/bin:$PATH
 export PATH=$PATH:/usr/local/go/bin
 export PATH=/home/rojetsavage/go:$PATH
 export PATH=/home/rojetsavage/go/bin:$PATH
+
+#node version manager
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
