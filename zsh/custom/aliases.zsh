9eadd284f4f15301ddeae71cf9d26f20dc0a9dcc
RojetSavage <robert-hurley@hotmail.com.au>
2024-04-01 12:48:58 +1100

    update

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -10,11 +10,14 @@ alias f='cd "$(find ~ ~/Uni ~/Projects ~/Programming -mindepth 1 -maxdepth 3 -ty
 #misc
 alias c='clear'
 alias ls='ls -l'
+alias echoServer='node ~/Projects/echoServer/index.js'
+alias uu='sudo apt update && sudo apt upgrade'
 
 #tmux
 alias tk='tmux kill-server'
 alias tl='tmux list-sessions'
 alias t='tmux attach'
+alias ts='tmux source-file ~/.config/tmux/tmux.conf'
 
-alias echoServer='node ~/Projects/echoServer/index.js'
-alias uu='sudo apt update && sudo apt upgrade'
+
+
