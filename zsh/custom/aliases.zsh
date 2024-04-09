2bf79fd1ebeb6cdbd08aa074efc484f674d74bc6
RojetSavage <robert-hurley@hotmail.com.au>
2024-04-10 07:56:30 +1000

    update

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,8 +1,10 @@
 #git
 alias ga='git add .'
+alias gupdate='git commit -m "update"'
 alias gl='git log --oneline --graph'
 alias gp='git push origin main'
 alias gs='git status'
+
 
 #navigation 
 alias f='cd "$(find ~ ~/Uni ~/Projects ~/Programming -mindepth 1 -maxdepth 3 -type d | fzf)"'
@@ -18,6 +20,7 @@ alias tk='tmux kill-server'
 alias tl='tmux list-sessions'
 alias t='tmux attach'
 alias ts='tmux source-file ~/.config/tmux/tmux.conf'k
+
 
 
 
