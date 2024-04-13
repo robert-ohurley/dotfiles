9750678a9f8dde8e2940ef674d4ab2bd74774874
robert-ohurley <robert-hurley@hotmail.com.au>
2024-04-13 14:44:09 +1000

    remove precmd

--- zsh/custom/functions.zsh
+++ zsh/custom/functions.zsh
@@ -1,9 +1,3 @@
-#precmd runs before each command but only after the first command
-precmd() {
-	
-}
-
-#simple add to zsh path
 add2Path() {
 	echo "export PATH=$1:\$PATH" >> $ZSH/custom/path.zsh
 }
