42ac629747ad603d744a605c741cb6810c006755
robert-ohurley <robert-hurley@hotmail.com.au>
2024-04-13 15:15:16 +1000

    fix path syntax

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -1,2 +1,7 @@
-path=("$HOME/Programming/scripts/" "$HOME/Projects/scripts" "$HOME/root/.local/bin" "$HOME/rojetsavage/.local/bin" "$HOME/usr/local/go/bin" "$XDG_DATA_HOME/go")
-export path
+export PATH=~/Programming/scripts/:$PATH
+export PATH=~/Projects/scripts:$PATH
+export PATH=/home//root/.local/bin:$PATH
+export PATH=/home/rojetsavage/.local/bin:$PATH
+export PATH=$PATH:/usr/local/go/bin
+export PATH=/home/rojetsavage/go:$PATH
+export PATH=/home/rojetsavage/go/bin:$PATH
