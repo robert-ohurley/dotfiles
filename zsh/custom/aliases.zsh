a4218fd78be3b03a2fd96a64e654f573ddd31719
robert-ohurley <robert-hurley@hotmail.com.au>
2024-04-25 16:18:06 +1000

    add nvim aliases

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -3,26 +3,32 @@ alias ga='git add .'
 alias gupdate='git commit -m "update"'
 alias gl='git log --oneline --graph'
 alias gp='git push origin main'
-alias gpull='git push origin main'
+alias gpull='git pull origin main'
 alias gs='git status'
 alias gsub='git submodule foreach git pull origin main'
-alias gogit="git add . && git commit -m 'update' && git push origin main"
+alias gg="git add . && git commit -m 'update' && git push origin main"
+alias gd="git diff --staged --color-words"
 
 #navigation 
-alias f='cd "$(find ~ ~/Uni ~/Projects ~/Programming -mindepth 1 -maxdepth 3 -type d | fzf)"'
+#alias f is a function
 
-#misc
+#misc'
 alias c='clear'
 alias ls='ls -l'
 alias echoServer='node ~/Projects/echoServer/index.js'
 alias uu='sudo apt update && sudo apt upgrade'
 
+#nvim
+alias vim='nvim'
+alias v='nvim'
+alias n='nvim'
+
 #tmux
-alias t='tmux'
+#alias t is a function
 alias tk='tmux kill-server'
-alias tl='tmux list-sessions'
+alias tls='tmux list-sessions'
 alias ta='tmux attach'
-alias ts='tmux source-file ~/.config/tmux/tmux.conf'
+alias tq='tmux detach'
 
 #prevent generation of dotfiles in $HOME - tidy up
 alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts" # Alias wget to use a custom hsts cache file location
@@ -30,4 +36,4 @@ alias nvidia-settings=nvidia-settings --config="$XDG_CONFIG_HOME"/nvidia/setting
 
 #sourcing 
 alias szsh='source $XDG_CONFIG_HOME/zsh/.zshrc'
-alias stmux='tmux source-file ~/.config/tmux/tmux.conf'
+alias stmux='tmux source-file $XDG_CONFIG_HOME/tmux/tmux.conf'
