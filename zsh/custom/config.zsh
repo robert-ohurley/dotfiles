20f9a701f3250aebe9e477e529e55f4e518e1cfa
robert-ohurley <robert-hurley@hotmail.com.au>
2024-04-25 16:17:47 +1000

    add nvm to path

--- zsh/custom/config.zsh
+++ zsh/custom/config.zsh
@@ -17,3 +17,7 @@ export ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST
 #compinit -d $ZSH/cache/.zcompdump
 #compinit -d "$XDG_CACHE_HOME"/zsh/zcompdump-"$ZSH_VERSION" 
 
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
