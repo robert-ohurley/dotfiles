fa2686375e6f0de93e06fb9b8dff107c86cd952c
robert-ohurley <robert-hurley@hotmail.com.au>
2024-04-25 16:17:12 +1000

    add tmux functions

--- zsh/custom/functions.zsh
+++ zsh/custom/functions.zsh
@@ -1,4 +1,22 @@
-add2Path() {
+addPath() {
 	echo "export PATH=$1:\$PATH" >> $ZSH/custom/path.zsh
+}
+
+#creates and attaches to a new tmux sesh if one doesn't already exist
+#i'm a monogamous tmux session kinda guy
+t() {
+
+	if [ -z $(tls | grep "sesh") ]
+	then
+		tmux new -s sesh
+	else
+		tmux attach -t sesh
+	fi
+}
+
+#simple navigation
+f() {
+	selected_directory=$(find ~/ -maxdepth 4 \( -path '*/.local/*' -o -path '*/.cache/*' -o -path '*/.config/*' -o -path '*/node_modules/*' \) -prune -o -type d -print | fzf)
+	cd "$selected_directory"
 }
 
