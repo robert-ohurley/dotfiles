6410a576ef147724f4fe0e42e577db99652b8916
robert-ohurley <robert-hurley@hotmail.com.au>
2024-05-04 08:03:53 +1000

    simplify tmux sessionizer

--- zsh/custom/functions.zsh
+++ zsh/custom/functions.zsh
@@ -2,18 +2,17 @@ addPath() {
 	echo "export PATH=$1:\$PATH" >> $ZSH/custom/path.zsh
 }
 
-#creates and attaches to a given tmux sesh if one doesn't already exist
-ts() {
-	if [ -z $1]
-	then
-		echo "Provide tmux session name"
-		return 0  
-	elif [ -z $(tls | grep "$1") ]
-	then
-		tmux new -s $1
-	else
-		tmux attach -t $1
-	fi
+t() {
+
+selected=$(find ~/ -maxdepth 4 \( -path '*/.local/*' -o -path '*/.cache/*' -o -path '*/.config/*' -o -path '*/node_modules/*' \) -prune -o -type d -print | fzf)
+
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+selected_name=$(basename "$selected" | tr . _)
+#Start a new session or attach to an existing session named $selected_name
+tmux new-session -A -s $selected_name
 }
 
 #simple navigation
