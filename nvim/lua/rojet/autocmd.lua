1c6b0e3de1a73be1b2a27f953ceff503e559a739
robert-ohurley <robert-hurley@hotmail.com.au>
2024-05-10 16:30:32 +1000

    update

--- nvim/lua/rojet/autocmd.lua
+++ nvim/lua/rojet/autocmd.lua
@@ -1,4 +1,6 @@
 -- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
@@ -7,7 +9,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-vim.api.nvim_create_autocmd({
+vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
   callback = function(event)
     local map = function(keys, func, desc)
@@ -42,7 +44,7 @@ vim.api.nvim_create_autocmd({
       })
     end
   end
-}, 'LspAttach')
+})
 
 vim.api.nvim_create_autocmd('LspDetach', {
   group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
