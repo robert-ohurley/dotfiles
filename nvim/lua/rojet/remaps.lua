55ef7da623761e249c2c40090dd943d46180c9f4
robert-ohurley <robert-hurley@hotmail.com.au>
2024-05-13 17:18:08 +1000

    .go buffer specific keymaps

--- nvim/lua/rojet/remaps.lua
+++ nvim/lua/rojet/remaps.lua
@@ -1,27 +1,32 @@
--- KEYMAPS
+-- KEYMAPS 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Remove highlighted search' } )
 vim.keymap.set('i', 'jk', '<Esc>')
 
 -- i'm going to get fired for these
-vim.keymap.set('i', '<C-]>', '<Esc>?{<CR><Esc>da}a{<CR>}<Esc>ko', { desc = 'after closing curly bracket, position cursor inside with correct formatting' } )
-vim.keymap.set('n', '<leader>q', vim.cmd.quit, { desc = 'Finally figured it out' } )
+vim.keymap.set('n', '<leader>q', vim.cmd.quita, { desc = 'Finally figured it out' } )
 vim.keymap.set('n', '<leader>w', vim.cmd.w, { desc = 'Write file' } )
 
--- Oof, v important for go, bad for anything else --
-vim.keymap.set('i', '::', ':=', { desc = 'Shortcut for walrus operator'} )
-vim.keymap.set('n', '<leader>ee', "iif err != nil {\n\t fmt.Println(err)\n}", { desc = 'Handle those errors'} )
+-- Golang laziness
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {"go"},
+  callback = function(event)
+    vim.keymap.set("i", "::", ":=", { buffer = event.buf, desc = "Walrus Operator" })
+    vim.keymap.set('n', '<leader>ee', "iif err != nil {\n\t fmt.Println(err)\n}", { buffer = event.buf, desc = "Handle those errors" } )
+  end,
+})
+
+-- Now that's what I call being lazy
+vim.keymap.set('i', '<C-]>', '<Esc>?{<CR><Esc>da}a{<CR>}<Esc>ko', { desc = 'after closing curly bracket, position cursor inside with correct formatting' })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [d]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [d]iagnostic message' })
+vim.keymap.set('n', '<leader>d', vim.lsp.buf.code_action, { desc = 'Show available code actions' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [e]rror messages' })
 vim.keymap.set('n', '<leader>Q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- I'm scared of the blackhole register
 vim.keymap.set('v', '<leader>p', '"_dp', { desc = 'Delete to blackhole register when pasting' })
-vim.keymap.set('v', '<leader>P', '"_dP', { desc = 'Delete to blackhole register when pasting' })
-
--- Faster up/down navigation
 vim.keymap.set('n', 'J', '5j')
 vim.keymap.set('n', 'K', '5k')
 vim.keymap.set('n', '<leader>o', 'o<Esc>', { desc = 'Insert new line below. Stay in normal mode' })
@@ -37,6 +42,11 @@ vim.keymap.set('n', '<leader>cn', ':cnext<CR>', { desc = 'Next item on quickfix 
 vim.keymap.set('n', '<leader>cp', ':cprev<CR>', { desc = 'Previous item on quickfix list' })
 
 --  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the above window' })
 
 -- terminal 
 vim.keymap.set('n', '<leader>x', ':!chmod +x %<CR>', { desc = 'Make current file e[x]ecutable' })
+
