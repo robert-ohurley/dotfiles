35ae6e4b3859a3f91fdaeeecf7b8f214cfd3c946
robert-ohurley <robert-hurley@hotmail.com.au>
2024-05-21 08:42:41 +1000

    add lazygit remap

--- nvim/lua/rojet/remaps.lua
+++ nvim/lua/rojet/remaps.lua
@@ -1,22 +1,28 @@
--- KEYMAPS 
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Remove highlighted search' } )
+-- KEYMAPS
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Remove highlighted search' })
 vim.keymap.set('i', 'jk', '<Esc>')
 
 -- i'm going to get fired for these
-vim.keymap.set('n', '<leader>q', vim.cmd.quita, { desc = 'Finally figured it out' } )
-vim.keymap.set('n', '<leader>w', vim.cmd.w, { desc = 'Write file' } )
+vim.keymap.set('n', '<leader>q', vim.cmd.q, { desc = 'Finally figured it out' })
+vim.keymap.set('n', '<leader>w', vim.cmd.w, { desc = 'Write file' })
+
+vim.keymap.set("n", "<leader>cs", "<cmd>Telescope colorscheme<CR>")
+vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<CR>")
 
 -- Golang laziness
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = {"go"},
+  pattern = { "go" },
   callback = function(event)
     vim.keymap.set("i", "::", ":=", { buffer = event.buf, desc = "Walrus Operator" })
-    vim.keymap.set('n', '<leader>ee', "iif err != nil {\n\t fmt.Println(err)\n}", { buffer = event.buf, desc = "Handle those errors" } )
+    vim.keymap.set('n', '<leader>ee', "iif err != nil {\n\t fmt.Println(err)\n}",
+      { buffer = event.buf, desc = "Handle those errors" })
   end,
 })
 
 -- Now that's what I call being lazy
-vim.keymap.set('i', '<C-]>', '<Esc>?{<CR><Esc>da}a{<CR>}<Esc>ko', { desc = 'after closing curly bracket, position cursor inside with correct formatting' })
+vim.keymap.set('i', '<C-]>', '<Esc>?{<CR><Esc>da}a{<CR>}<Esc>ko',
+  { desc = 'after closing curly bracket, position cursor inside with correct formatting' })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [d]iagnostic message' })
@@ -40,13 +46,10 @@ vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = "Move highlighted lines do
 vim.keymap.set('n', '<leader>ft', vim.cmd.NvimTreeToggle, { desc = 'View [f]ile [t]ree' })
 vim.keymap.set('n', '<leader>cn', ':cnext<CR>', { desc = 'Next item on quickfix list' })
 vim.keymap.set('n', '<leader>cp', ':cprev<CR>', { desc = 'Previous item on quickfix list' })
-
---  See `:help wincmd` for a list of all window commands
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the above window' })
 
--- terminal 
+-- terminal
 vim.keymap.set('n', '<leader>x', ':!chmod +x %<CR>', { desc = 'Make current file e[x]ecutable' })
-
