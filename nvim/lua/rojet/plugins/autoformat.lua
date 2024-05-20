8e10ef6c8b88b4206aeebe1fccd025cf98133ec9
robert-ohurley <robert-hurley@hotmail.com.au>
2024-05-21 08:42:27 +1000

    disable vue autofmt on save

--- nvim/lua/rojet/plugins/autoformat.lua
+++ nvim/lua/rojet/plugins/autoformat.lua
@@ -4,7 +4,7 @@ return { -- Autoformat
   lazy = false,
   keys = {
     {
-      '<leader>fm',
+      '<leader>FM',
       function()
         require('conform').format { async = true, lsp_fallback = true }
       end,
@@ -18,7 +18,7 @@ return { -- Autoformat
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true, sql = true, lua = true, javascript = true, vue = true }
+      local disable_filetypes = { c = true, cpp = true, sql = true, lua = true, javascript = true, vue = false }
       return {
         timeout_ms = 500,
         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
