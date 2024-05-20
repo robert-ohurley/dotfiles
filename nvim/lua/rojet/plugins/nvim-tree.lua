7180a5cad2fc1e074b80712bbac56b98e11667b3
robert-ohurley <robert-hurley@hotmail.com.au>
2024-05-21 08:40:56 +1000

    open tree in directory of current buffer

--- nvim/lua/rojet/plugins/nvim-tree.lua
+++ nvim/lua/rojet/plugins/nvim-tree.lua
@@ -3,21 +3,29 @@ return {
   lazy = false,
   config = function()
     require("nvim-tree").setup({
-    view = {
-      width = 30,
-    },
-    renderer = {
-      group_empty = true,
-      icons = {
+      view = {
+        width = 30,
+      },
+
+      --opens nvim tree wrt current buffer
+      update_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_cwd = true,
+      },
+
+      renderer = {
+        group_empty = true,
+        icons = {
           git_placement = 'after',
           show = {
             git = false
           }
+        },
       },
-    },
-    filters = {
-      dotfiles = true,
-    },
-  })
+      filters = {
+        dotfiles = true,
+      },
+    })
   end
 }
