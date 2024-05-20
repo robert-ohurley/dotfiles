d7f6f36fe540d5a51fd78dd7a0d9453b276c50e1
robert-ohurley <robert-hurley@hotmail.com.au>
2024-05-21 08:40:23 +1000

    add lazygit

--- nvim/lua/rojet/lazy-plugins.lua
+++ nvim/lua/rojet/lazy-plugins.lua
@@ -1,22 +1,6 @@
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-  --
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
   require 'rojet.plugins.autoformat',
@@ -26,6 +10,7 @@ require('lazy').setup({
   require 'rojet.plugins.fugitive',
   require 'rojet.plugins.gitsigns',
   require 'rojet.plugins.harpoon',
+  require 'rojet.plugins.lazygit',
   require 'rojet.plugins.lsp',
   require 'rojet.plugins.misc',
   require 'rojet.plugins.nvim-tmux-navigator',
@@ -42,8 +27,6 @@ require('lazy').setup({
   -- require 'kickstart.plugins.neo-tree',
 }, {
   ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
     icons = vim.g.have_nerd_font and {} or {
       cmd = '⌘',
       config = '🛠',
