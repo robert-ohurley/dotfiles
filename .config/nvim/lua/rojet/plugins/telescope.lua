2ca4c2eea7bc807c602c32c0ad6f1798e50877f9
robert-ohurley <robert-hurley@hotmail.com.au>
2024-05-30 17:46:44 +1000

    set telescope to search hidden files excluding git

--- .config/nvim/lua/rojet/plugins/telescope.lua
+++ .config/nvim/lua/rojet/plugins/telescope.lua
@@ -70,7 +70,7 @@ return
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>")
       vim.keymap.set('n', '<leader>sg', builtin.git_files, { desc = '[S]earch [G]it Files (Tracked by git)' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.live_grep, { desc = '[S]earch by [W]ord with Grep' })
