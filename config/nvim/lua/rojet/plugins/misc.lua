b87f5c8e09dbfc7d08fc64376d7f791b6430ea6e
robert-ohurley <robert-hurley@hotmail.com.au>
2024-06-15 09:41:49 +1000

    remove comments

--- config/nvim/lua/rojet/plugins/misc.lua
+++ config/nvim/lua/rojet/plugins/misc.lua
@@ -18,23 +18,25 @@ return {
       require('mini.surround').setup()
 
       -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
         return '%2l:%-2v'
       end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
-}
 
+  {
+    -- gco - Comment below
+    -- gcO - Comment above
+    -- gcA - Comment at the end of the line
+    -- gc - Line-comment
+    -- gb - Block-comment
+    'numToStr/Comment.nvim',
+    lazy = false,
+  },
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+
+}
