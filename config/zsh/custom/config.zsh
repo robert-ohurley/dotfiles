967cd25c8cb0bf7888252456617f3e52be7ffbed
robert-ohurley <robert-hurley@hotmail.com.au>
2024-06-15 09:40:15 +1000

    lazy load nvm

--- config/zsh/custom/config.zsh
+++ config/zsh/custom/config.zsh
@@ -3,10 +3,5 @@ HISTFILE=$ZSH/.zsh_history
 
 #Prevent creation of some of the ZSH_COMPDUMP files
 export ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST
-#compinit -d $ZSH/cache/.zcompdump
-#compinit -d "$XDG_CACHE_HOME"/zsh/zcompdump-"$ZSH_VERSION" 
 
-export NVM_DIR="$HOME/.config/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
