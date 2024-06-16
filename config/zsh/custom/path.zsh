3d31dca31b818a76e5948d7b4659ed8698aea0b1
robert-ohurley <robert-hurley@hotmail.com.au>
2024-06-16 17:03:11 +1000

    properly set STARSHIP_CONFIG variable

--- config/zsh/custom/path.zsh
+++ config/zsh/custom/path.zsh
@@ -1,5 +1,4 @@
 export HOME=/home/rojetsavage
-export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/starship.toml
 
 #XDG paths
 export XDG_DATA_HOME=$HOME/.local/share
@@ -24,6 +23,7 @@ export _JAVA_OPTIONS=-Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java
 export NODE_REPL_HISTORY="$XDG_DATA_HOME"/node_repl_history 
 export NVM_DIR=$XDG_DATA_HOME/nvm
 export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
+export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/starship.toml
 
 # Default Apps
 export BROWSER="brave-browser"
