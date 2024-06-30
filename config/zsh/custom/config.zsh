dd75c20cfe2ff420e8ac56d596cf470e6f922124
robert-ohurley <robert-hurley@hotmail.com.au>
2024-06-30 17:00:10 +1000

    fix zsh history bug

--- config/zsh/custom/config.zsh
+++ config/zsh/custom/config.zsh
@@ -1,5 +1,5 @@
 #Moves zsh history location
-HISTFILE=$ZSH/.zsh_history
+HISTFILE=$XDG_CONFIG_HOME/zsh/.zsh_history
 
 #Prevent creation of some of the ZSH_COMPDUMP files
 export ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST
