3ae7788915bbc373a566a2ae5c7285f8a32559df
robert-ohurley <robert-hurley@hotmail.com.au>
2024-08-01 07:44:40 +1000

    add lsp for json files

--- config/nvim/lua/rojet/plugins/lsp.lua
+++ config/nvim/lua/rojet/plugins/lsp.lua
@@ -35,16 +35,16 @@ return {
       local servers = {
         -- clangd = {},
         gopls = {},
-        bashls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        bashls = {
+         filetypes = { "bash" },
+        },
+        jsonls = {
+         filetypes = { "json" },
+        },
+        pyright = {},
+        tsserver = {
+          filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
+        },
         volar = {
           filetypes = { "vue" }
         },
@@ -71,10 +71,14 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        'stylua',
         'gopls',
         'tsserver',
+        'pyright',
+        'html',
         'bashls',
+        'jsonls',
+
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
