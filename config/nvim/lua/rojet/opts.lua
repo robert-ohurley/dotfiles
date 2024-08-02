11d9db695c00e98225d6b2c4db89f58f29f49ab3
robert-ohurley <robert-hurley@hotmail.com.au>
2024-08-03 08:44:52 +1000

    increase timeout len

--- config/nvim/lua/rojet/opts.lua
+++ config/nvim/lua/rojet/opts.lua
@@ -45,9 +45,7 @@ vim.opt.signcolumn = 'yes'
 -- Decrease update time
 vim.opt.updatetime = 250
 
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
+vim.opt.timeoutlen = 1000
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
