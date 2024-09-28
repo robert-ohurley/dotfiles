1f7850ba80652fed8632ba0c13781425bf150cd4
robert-ohurley <robert-hurley@hotmail.com.au>
2024-09-29 08:40:58 +1000

    tidy up duplicates

--- config/zsh/custom/config.zsh
+++ config/zsh/custom/config.zsh
@@ -4,4 +4,9 @@ HISTFILE=$XDG_CONFIG_HOME/zsh/.zsh_history
 #Prevent creation of some of the ZSH_COMPDUMP files
 export ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST
 
+#prevent generation of dotfiles in $HOME - tidy up
+alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts" 
+alias nvidia-settings=nvidia-settings --config="$XDG_CONFIG_HOME"/nvidia/settings
+export PYTHON_HISTORY="$XDG_CONFIG_HOME"/python
+
 
