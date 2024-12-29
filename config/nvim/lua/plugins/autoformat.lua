52e194418b7e7dcc9ae9ca8b988078bfc77f591b
robert-ohurley <robert-hurley@hotmail.com.au>
2024-12-30 07:42:04 +1100

    refactor

--- config/nvim/lua/plugins/autoformat.lua
+++ config/nvim/lua/plugins/autoformat.lua
@@ -9,30 +9,30 @@ return { -- Autoformat
         require('conform').format { async = true, lsp_fallback = true }
       end,
       mode = '',
-      desc = '[F]or[m]at buffer',
+      desc = '[f]or[m]at buffer',
     },
   },
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true, lua = true, javascript = true, vue = true }
+      local disable_filetypes = { c = true, cpp = true }
       return {
         timeout_ms = 500,
         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
       }
     end,
     formatters_by_ft = {
+      -- Run multiple formatters sequentially
+      go = { 'goimports', 'gofmt' },
       lua = { 'stylua' },
-      -- Conform can also run multiple formatters sequentially
       python = { 'isort', 'black' },
       sql = { 'sql-formatter' },
-      --
-      -- You can use a sub-list to tell conform to run *until* a formatter
-      -- is found.
-      -- javascript = { { "prettierd", "prettier" } },
+      -- javascript = { 'prettier' },
+      -- The "*" filetype runs formatters on all filetypes.
+      ['*'] = { 'codespell' },
+      -- The "_" filetype runs formatters on filetypes that don't have other formatters configured.
+      ['_'] = { 'trim_whitespace' },
     },
+    notify_no_formatters = true,
   },
 }
--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -6,18 +6,44 @@ return {
   config = function()
     local harpoon = require 'harpoon'
 
-    harpoon.setup()
+    harpoon:setup()
+
+    -- basic telescope configuration
+    local conf = require('telescope.config').values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require('telescope.pickers')
+        .new({}, {
+          prompt_title = 'Harpoon',
+          finder = require('telescope.finders').new_table {
+            results = file_paths,
+          },
+          previewer = conf.file_previewer {},
+          sorter = conf.generic_sorter {},
+        })
+        :find()
+    end
+
+    vim.keymap.set('n', '<leader>h', function()
+      toggle_telescope(harpoon:list())
+    end, { desc = 'Open harpoon window' })
 
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
     end, { desc = 'Add to Harpoon' })
-    vim.keymap.set('n', '<leader>h', function()
+
+    vim.keymap.set('n', '<C-e>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = 'Toggle Harpoon' })
 
     vim.keymap.set('n', '<A-h>', function()
       harpoon:list():prev()
     end, { desc = 'Prev Harpoon' })
+
     vim.keymap.set('n', '<A-l>', function()
       harpoon:list():next()
     end, { desc = 'Next Harpoon' })
