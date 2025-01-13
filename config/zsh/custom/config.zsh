ae98844aabdf48e495518f8991135ce3d447465d
robert-ohurley <robert-hurley@hotmail.com.au>
2025-01-13 21:09:47 +1100

    update path

--- config/zsh/custom/config.zsh
+++ config/zsh/custom/config.zsh
@@ -5,7 +5,7 @@ HISTFILE=$XDG_CONFIG_HOME/zsh/.zsh_history
 export ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST
 
 #prevent generation of dotfiles in $HOME - tidy up
-alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts" 
+alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
 alias nvidia-settings=nvidia-settings --config="$XDG_CONFIG_HOME"/nvidia/settings
 export PYTHON_HISTORY="$XDG_CONFIG_HOME"/python
 
